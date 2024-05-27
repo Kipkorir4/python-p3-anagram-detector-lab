431f7fab612f98969a13d1cf00fcf3780a53bc1e
Gideon <keterjr1@gmail.com>
2024-05-27 08:18:51 +0300

    pass tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+    
+    def match(self, possible_anagrams):
+        return [anagram for anagram in possible_anagrams if sorted(anagram.lower()) == self.sorted_word]
